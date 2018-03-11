0314ead741c07cf03fc8b62e652486dcdb5e07ef
花夏 <liubiao0810@live.cn>
2018-03-11 23:07:40 +0800

    update练习代码

--- Lesson-1/assignment/yours.sol
+++ Lesson-1/assignment/yours.sol
@@ -1,1 +1,16 @@
-/*作业请提交在这个目录下*/
+// 声明所使用的版本，实际上是方便开源
+pragma solidity ^0.4.14;
+
+contract CompensationSys {
+    // 声明一个智能合约，这个智能合约是薪酬系统
+    uint storedData;
+    
+    // 设置值 
+    function setStoredData(uint x) {
+        storedData = x;
+    }
+    // 获取值
+    function getStoredData() constant returns(uint) {
+        return storedData;
+    }
+}
