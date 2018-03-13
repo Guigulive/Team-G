a46c3eee585ac1e1a06cd6464ffc736244fbca6a
花夏 <liubiao0810@live.cn>
2018-03-13 10:40:52 +0800

    更新代码

--- Lesson-1/assignment/yours.sol
+++ Lesson-1/assignment/yours.sol
@@ -23,17 +23,26 @@ contract CompensationSys {
     uint lastPayDay = now;
     address owner;
 
-    // 这是构造函数？智能合约一部署自动执行
-    // 然后将所有者赋给 owner？
+    //**
+    //* [CompensationSys 这是构造函数？智能合约一部署自动执行然后将所有者赋给 owner？]
+    //* @method   CompensationSys
+    //* @author 花夏 liubiao@itoxs.com
+    //* @datetime 2018-03-13T10:38:31+080
+    //*/
     function CompensationSys() {
         owner = msg.sender;
     }
 
-    // updateEmployeeMsg   更新员工地址或者月薪基数
-    // @param ads          地址，可以为空
-    // @param sly          更新的月薪基数可以为空
+    //**
+    //* [updateEmployeeMsg 更新员工地址或者月薪基数]
+    //* @method   updateEmployeeMsg
+    //* @author 花夏 liubiao@itoxs.com
+    //* @datetime 2018-03-13T10:21:58+080
+    //* @param    {address}               ads [更新地址]
+    //* @param    {uint}                  sly [更新的月薪基数]
     // *** 填写地址处一定要使用英文状态下的双引号 "" ***
     // 例如 "0xca35b7d915458ef540ade6068dfe2f44e8fa733c"
+    //*/
     function updateEmployeeMsg(address ads, uint sly) {
         // 检查是否合约所有者，不是的话是不允许改变的哦，要不然嘻嘻嘻~~~
         require(msg.sender == owner);
@@ -47,6 +56,9 @@ contract CompensationSys {
         // 这个函数是用作更新员工信息的，为什么要一个函数做多件事情？
         // 所以我省去了~~~
 
+        // 看了群里的解释，貌似自己没有想到，更新地址后是需要支付没有支付的薪资的
+        // 私认为还是单独写一个函数设置吧
+        paySurplusWages();
         // 天啊撸！ 老板你调皮了~~ 万一执行者傻乎乎的没写地址呢？
         // 是不是要兼容一下？嘻嘻嘻~~~
         // if (ads == 0x0) {
@@ -68,20 +80,49 @@ contract CompensationSys {
         lastPayDay = now;
     }
     
-    // addFund 添加支付金到合约地址
-    // payable 这个必须声明，因为不声明是无法增加金额进去的
-    // return  返回合约地址的总金额
+    //**
+    //* [paySurplusWages 在更新地址或者月薪时支付剩余余额]
+    //* @method   paySurplusWages
+    //* @author 花夏 liubiao@itoxs.com
+    //* @datetime 2018-03-13T10:18:51+080
+    //*/
+    function paySurplusWages() {
+        if (employee != 0x0) {
+             uint paySurplusWages = salary * (now - lastPayDay) / payStep;
+             employee.transfer(paySurplusWages);
+        }
+    }
+    
+    //**
+    //* [addFund 添加支付金到合约地址]
+    //* @method   addFund
+    //* @author   花夏 liubiao@itoxs.com
+    //* @datetime 2018-03-13T10:20:27+080
+    //* @return   返回合约的余额
+    //*/
     function addFund() payable returns(uint) {
         // this 指向合约对象
         return this.balance;
     }
-    
-    // getPayTimes 获取合约地址还能支付薪水的次数
-    // return      返回合约地址还能支付薪水的次数
+
+    //**
+    //* [getPayTimes 获取合约地址还能支付薪水的次数]
+    //* @method   getPayTimes
+    //* @author 花夏 liubiao@itoxs.com
+    //* @datetime 2018-03-13T10:24:27+080
+    //* @return   {uint}                [返回合约地址还能支付薪水的次数]
+    //*/
     function getPayTimes() returns(uint) {
         return this.balance / salary;
     }
 
+    //**
+    //* [hasEnoughPay 查看是否支付足够]
+    //* @method   hasEnoughPay
+    //* @author 花夏 liubiao@itoxs.com
+    //* @datetime 2018-03-13T10:30:36+080
+    //* @return   {Boolean}               [返回是否足够支付]
+    //*/
     function hasEnoughPay() returns(bool) {
         // ether == 10^18 wei
         // finney == 10^15 wei
@@ -93,7 +134,12 @@ contract CompensationSys {
         return this.balance >= salary + 10 finney;
     }
 
-    // getMyWage 获取我应得的薪资
+    //**
+    //* [getMyWage 员工自己领取自己的工资]
+    //* @method   getMyWage
+    //* @author 花夏 liubiao@itoxs.com
+    //* @datetime 2018-03-13T10:32:35+080
+    //*/
     function getMyWage() {
         if (msg.sender != employee) {
             // 龟毛的花夏也是个好人啊，你们可以来随便点，不会浪费你们的手续费；
