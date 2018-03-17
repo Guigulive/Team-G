678954fa4fe3877cc94f8b631a853f3e6e34b992
kevin song <kevin.song@optus.com.au>
2018-03-18 00:15:25 +1100

    61_宋兴超_第二次作业

--- Lesson-2/assignment/README.md
+++ Lesson-2/assignment/README.md
@@ -4,7 +4,21 @@
 ### 第二课：课后作业
 完成今天的智能合约添加100ETH到合约中
 - 加入十个员工，每个员工的薪水都是1ETH
-每次加入一个员工后调用calculateRunway这个函数，并且记录消耗的gas是多少？Gas变化么？如果有 为什么？
-- 如何优化calculateRunway这个函数来减少gas的消耗？
+每次加入一个员工后调用calculateRunway这个函数，并且记录消耗的gas是多少？Gas变化么？如果有 - 如何优化calculateRunway这个函数来减少gas的消耗？
+
+简单的记录了一下2个，3个，4个员工的时候 calculateRunway 消耗的gas， 可以看到gas使用明显增大。因为for循环随着数组的增加，消耗的也越来越多，可以通过一个变量来保存所有员工工资的和，每次增加删除员工的时候来更改这个和
+
+ 2 employee	
+ transaction cost 	23747 gas 
+ execution cost 	2475 gas 
+3
+ transaction cost 	24528 gas 
+ execution cost 	3256 gas 
+ 4
+ transaction cost 	25309 gas 
+ execution cost 	4037 gas 
+
 提交：智能合约代码，gas变化的记录，calculateRunway函数的优化
+
+
 
--- Lesson-2/assignment/yours.sol
+++ Lesson-2/assignment/yours.sol
@@ -1,1 +1,82 @@
 /*作业请提交在这个目录下*/
+pragma solidity ^0.4.14;
+contract Payroll{
+    struct Employee {
+        address id;
+        uint salary;
+        uint lastPayDay;
+    }
+    uint  payDuration = 10 seconds;
+    address owner;
+    Employee[] employees;
+    uint salarySum;
+    
+    function Payroll(){
+        owner = msg.sender;
+    }
+    
+    function _partialPaid(Employee employee) private{
+        uint payAmount = employee.salary * (now - employee.lastPayDay) / payDuration;
+        employee.id.transfer(payAmount);
+    }
+    
+    function _findEmployee(address employeeId) private returns (Employee, uint) {
+        for(uint i = 0; i < employees.length; i++){
+            if(employees[i].id == employeeId){
+                return (employees[i], i);
+            }
+        }
+    }
+    
+    function addEmployee(address employeeId, uint salary){
+        require(msg.sender == owner);
+        var(employee, index) = _findEmployee(employeeId);
+        assert(employee.id == 0x0);
+        employees.push(Employee(employeeId, salary * 1 ether,now));
+        salarySum += salary * 1 ether;
+    }
+    
+    function removeEmployee(address employeeId){
+        require(msg.sender == owner);
+        var (employee, index) = _findEmployee(employeeId);
+        assert(employee.id != 0x0);
+        _partialPaid(employee);
+        delete employees[index];
+        employees[index] = employees[employees.length -1];
+        employees.length -= 1;
+        salarySum -= employees.salary;
+    }
+    
+    function updateEmployee(address employeeId, uint salary){
+        require(msg.sender == owner);
+        var(employee, index) = _findEmployee(employeeId);
+        assert(employee.id != 0x0);
+        _partialPaid(employee);
+        employees[index].id = employeeId;
+        employees[index].salary = salary * 1 ether;
+        employees[index].lastPayDay = now;
+    }
+    
+      function caclulateRunway()  returns (uint){
+      return this.balance / salarySum;
+  }
+   
+   function addFund() public payable returns (uint){
+       return this.balance;
+   }
+
+  
+  function hasEnoughFund() private returns (bool) {
+      return caclulateRunway() > 0;
+  }
+  
+  function getPaid() public {
+     var (employee, index) = _findEmployee(msg.sender);
+     assert(employee.id != 0x0);
+     
+     uint nextPayday = employee.lastPayDay + payDuration;
+     assert(nextPayday < now);
+     employee.lastPayDay = nextPayday;
+     employee.id.transfer(employee.salary);
+  }
+}
