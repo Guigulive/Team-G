e60a4ecea0372e6fea255891059086e67a6ab20e
花夏 <liubiao0810@live.cn>
2018-03-25 21:44:00 +0800

    优化测试用例上传示例图

--- payroll/test/.payroll-test/getMyWage.test.js
+++ payroll/test/.payroll-test/getMyWage.test.js
@@ -4,25 +4,46 @@ const chalk = require('chalk');
 const _GAS = {
     gas: 3000000
 };
-module.exports = function(PayrollStorage, accounts, next) {
-    const spinner = ora('开始Payroll合约中' + chalk.cyan(' getMyWage ') + '函数测试!\n').start();
+module.exports = function(PayrollStorage, accounts, spinner, next) {
+    spinner.color = 'yellow';
+    spinner.text = '测试中...\n';
     next && next();
     var address = accounts[1];
     var PayrollStorageInstance;
-    it('', function () {
+    it('当前自己地址 执行getMyWage测试', function() {
         return PayrollStorage.deployed().then(function (instance) {
             PayrollStorageInstance = instance;
             return PayrollStorageInstance.addEmployee(address, 1, _.assign({
                 from: accounts[0]
             }, _GAS));
-        }).then(function () {
+        }).then(function() {
             return PayrollStorageInstance.getMyWage(_.assign({
                 from: address
             }, _GAS));
         }).then(function (storedData) {
-            spinner.color = 'green';
-            spinner.text = chalk.green('Function getMyWage 测试成功');
-            spinner.succeed();
+            setTimeout(()=> {
+                spinner.color = 'green';
+                spinner.text = chalk.green('Function getMyWage 成功');
+                spinner.succeed();
+            }, 100);
+        });
+    });
+    it('非自己地址 执行getMyWage测试', function() {
+        return PayrollStorage.deployed().then(function (instance) {
+            PayrollStorageInstance = instance;
+            return PayrollStorageInstance.addEmployee(address, 1, _.assign({
+                from: accounts[0]
+            }, _GAS));
+        }).then(function() {
+            return PayrollStorageInstance.getMyWage(_.assign({
+                from: address
+            }, _GAS));
+        }).catch(function() {
+            setTimeout(()=> {
+                spinner.color = 'green';
+                spinner.text = chalk.green('非自己地址测试 Function getMyWage 成功');
+                spinner.succeed();
+            }, 100);
         });
     });
 };
