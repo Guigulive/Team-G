28d045f9f81baafd3a06ab80e6c9d5ef1c97b2d8
花夏 <liubiao0810@live.cn>
2018-03-31 15:10:13 +0800

    结合vue优化首页

--- vue-payroll/src/app/controller/search.js
+++ vue-payroll/src/app/controller/search.js
@@ -2,26 +2,20 @@ import {BigNumber} from 'bignumber.js';
 import moment from 'moment';
 export default {
     searchOfAds(ads, _this) {
-        var Payroll = _this.Payroll;
         var web3 = _this.web3;
-        var PayrollInstance;
-        Payroll.deployed().then((instance) => {
-            PayrollInstance = instance;
-            var employeeInfo = PayrollInstance.employees.call(ads);
-            return employeeInfo;
-        }).then((result) => {
-            _this.loading = false;
-            _this.disabled = false;
-            let employeeAds = result[0];
+        _this.loading = false;
+        _this.disabled = false;
+        _this.instance.employees.call(ads).then(values => {
+            let employeeAds = values[0];
             // 判段地址是否是 0x0000000000000000000000000000000000000000
             if (parseInt(employeeAds.slice(2), 10) === 0) {
                 _this.result = 0;
                 return;
             }
             // salary
-            let salary = web3.fromWei(new BigNumber(result[1]).toNumber());
+            let salary = web3.fromWei(new BigNumber(values[1]).toNumber());
             // lastPayDay
-            let lastPayDay = moment(new Date(new BigNumber(result[2]).toNumber()) * 1000).format('LLLL');
+            let lastPayDay = moment(new Date(new BigNumber(values[2]).toNumber()) * 1000).format('LLLL');
             let employeeInfo = {
                 employeeAds: {
                     name: '员工地址',
